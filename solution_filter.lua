5d8ea1c1c1c4f87e05d753320ac54604ba67417c
unknown <g_d@gmx.ch>
2024-10-04 22:24:21 +0200

    Hinweisbox + cleanup

--- solution_filter.lua
+++ solution_filter.lua
@@ -1,6 +1,6 @@
 function Div(el)
   if el.classes[1] == "solution" then
-    local summary = pandoc.RawBlock("html", "<details>\n  <summary>Lösung</summary>")
+    local summary = pandoc.RawBlock("html", "<details>\n  <summary>Klicke hier, um deine Lösung zu überprüfen.</summary>")
     local endDetails = pandoc.RawBlock("html", "</details>")
     
     table.insert(el.content, 1, summary)
